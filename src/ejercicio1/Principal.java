12f5cdb01a7155385e8aaf09fcc19e8ab452ab24
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-29 13:22:03 +0100

    Ejercicio 1 acabado

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,7 +1,25 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Principal {
     static void main() {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuántos nombres quieres guardar?");
+        int numeroNombres = sc.nextInt();
+        String[] cantidad = new String[numeroNombres];
+        String nombre;
+        int numNombres=1;
+        System.out.println("Introduce los nombres que quieres guardar");
+
+        for (int i = 0; i < cantidad.length; i++) {
+            System.out.println("Nombre "+numNombres);
+            nombre = sc.next();
+            cantidad[i] = nombre;
+            numNombres++;
+        }
+        for (int i=cantidad.length- 1; i >= 0; i--) {
+            System.out.println("Posición "+ i + cantidad[i]);
+        }
     }
 }
