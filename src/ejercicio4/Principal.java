a556c888930bfbca3f2c68a74868a9d88838830f
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-11-06 12:08:05 +0100

    Ejercicio 4 acabado

--- src/ejercicio4/Principal.java
+++ src/ejercicio4/Principal.java
@@ -1,4 +1,19 @@
 package ejercicio4;
+import java.util.Arrays;
 
 public class Principal {
+    static void main() {
+        String[] equipoA = {"Messi", "Suárez", "Neymar"};
+        String [] equipoB = {"Xavi", "Iniesta"};
+        String[] equipoFusion = new String[equipoA.length + equipoB.length];
+
+        System.arraycopy(equipoA, 0, equipoFusion, 0, equipoA.length);
+        System.arraycopy(equipoB, 0, equipoFusion, equipoA.length, equipoB.length);
+
+        System.out.println("Equipo A: " + Arrays.toString(equipoA));
+        System.out.println("Equipo B: " + Arrays.toString(equipoB));
+        System.out.println("Equipo Fusión: " + Arrays.toString(equipoFusion));
+    }
 }
+
+
